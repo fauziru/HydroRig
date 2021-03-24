c7ab29c4deba940108db8b48d18d4ead6cf4dc72
fauziru <fauzirezaumr@gmail.com>
2021-03-24 22:05:03 +0700

    add oauth key

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,7 +28,6 @@ class AuthServiceProvider extends ServiceProvider
         Passport::routes();
         Passport::tokensExpireIn(now()->addDays(1));
         Passport::refreshTokensExpireIn(now()->addDays(3));
-        Passport::loadKeysFrom('/secret-keys/oauth');
         //
     }
 }
