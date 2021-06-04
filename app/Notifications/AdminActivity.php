2f7ea82cb1b70d63c558b7029bebcdde1c89628e
fauziru <fauzirezaumr@gmail.com>
2021-06-04 17:55:01 +0700

    bug fixes

--- app/Notifications/AdminActivity.php
+++ app/Notifications/AdminActivity.php
@@ -69,7 +69,7 @@ class AdminActivity extends Notification
     public function toBroadcast($notifiable): BroadcastMessage
     {
         return new BroadcastMessage([
-            'payload' => [
+            'data' => [
                 'data' => $this->item['message'],
                 'created_at' => Carbon::now(),
                 'link' => $this->item['link']
