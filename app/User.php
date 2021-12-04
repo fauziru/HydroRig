a4bbb8c7c1784f11915d98f2954e630568c218ad
Fauzi Umara <fauzirezaumr@gmail.com>
2021-12-04 23:24:05 +0700

    change fillable col db

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'role', 'phone'
+        'name_user', 'email', 'password', 'role_id', 'phone'
     ];
 
     /**
