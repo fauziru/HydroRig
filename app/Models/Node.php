5e8db437f3442f9b05ef761bf32845e0462fca88
Fauzi Umara <fauzirezaumr@gmail.com>
2021-12-04 23:26:43 +0700

    add new model for node and role

---
+++ app/Models/Node.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Uuid;
+use App\Traits\Blameable;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\Contracts\Activity;
+
+class Node extends Model
+{
+    use Uuid, LogsActivity, Blameable;
+
+    protected $table = 'nodes';
+
+    protected $fillable = ['name_node', 'status', 'konektivitas', 'lat', 'lng'];
+
+    protected static $logAttributes = ['name_node', 'konektivitas', 'lat', 'lng'];
+
+    public function sensors()
+    {
+        return $this->hasMany('App\Models\Sensor');
+    }
+
+    public function reads()
+    {
+        return $this->hasManyThrough('App\Models\Read', 'App\Models\Sensor', 'node_id', 'sensor_id');
+    }
+
+    public function resolveRouteBinding($value, $field = NULL)
+    {
+        return $this->where('uuid', $value)->first() ?? abort(404);
+    }
+
+    public function tapActivity(Activity $activity, string $eventName)
+    {
+        $activity->log_name = "Node";
+        $activity->description = "activity.logs.node.{$eventName}";
+    }
+}
---
+++ app/Models/Role.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Uuid;
+use App\Traits\Blameable;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\Contracts\Activity;
+
+class Role extends Model
+{
+    //
+    use Uuid, LogsActivity, Blameable;
+    
+    protected $table = 'roles';    
+
+    protected $fillable = ['name_role', 'create', 'read', 'update', 'delete'];
+
+    protected static $logAttributes = [];
+
+    public function resolveRouteBinding($value, $field = NULL)
+    {
+        return $this->where('uuid', $value)->first() ?? abort(404);
+    }
+
+    public function tapActivity(Activity $activity, string $eventName)
+    {
+        $activity->log_name = "Role";
+        $activity->description = "activity.logs.role.{$eventName}";
+    }
+}
