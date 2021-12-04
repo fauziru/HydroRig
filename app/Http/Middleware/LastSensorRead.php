30675b644584e9dfa828f8fca3e71bb0ae0152ba
Fauzi Umara <fauzirezaumr@gmail.com>
2021-12-04 23:30:58 +0700

    change import file name

--- app/Http/Middleware/LastSensorRead.php
+++ app/Http/Middleware/LastSensorRead.php
@@ -6,7 +6,7 @@ use Closure;
 use Carbon\Carbon;
 use Cache;
 use App\Models\Sensor;
-use App\Models\ReadNutrisi;
+use App\Models\Read;
 
 class LastSensorRead
 {
@@ -21,7 +21,7 @@ class LastSensorRead
     {
         $id = $request->route('sensor')['id'];
         if (!Cache::has('sensor-is-connect-' . $id)) {
-            $read = ReadNutrisi::create(['sensor_id' => $id, 'read_nutrisi' => 0]);
+            $read = Read::create(['sensor_id' => $id, 'read_nutrisi' => 0]);
             $read->created_at = $request->route('sensor')['last_read'];
             $read->save();
         }
